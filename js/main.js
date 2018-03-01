ebe66d95d821cd1dcb6954d393a3d647b1019b51
Angelo Dalisay <dalisay.angelo@gmail.com>
2018-03-01 09:27:01 -0700

    Initial push
    	new file:   400.shtml
    	new file:   401.shtml
    	new file:   403.shtml
    	new file:   404.shtml
    	new file:   500.php
    	new file:   500.shtml
    	new file:   css/bigvideo.css
    	new file:   css/bootstrap-theme.css
    	new file:   css/bootstrap-theme.css.map
    	new file:   css/bootstrap-theme.min.css
    	new file:   css/bootstrap.css
    	new file:   css/bootstrap.css.map
    	new file:   css/bootstrap.min.css
    	new file:   css/main.css
    	new file:   css/video-js.css
    	new file:   daylight.mp4
    	new file:   default.html
    	new file:   evening.mp4
    	new file:   favicon.ico
    	new file:   fonts/glyphicons-halflings-regular.eot
    	new file:   fonts/glyphicons-halflings-regular.svg
    	new file:   fonts/glyphicons-halflings-regular.ttf
    	new file:   fonts/glyphicons-halflings-regular.woff
    	new file:   img/bigvideo.png
    	new file:   index.html
    	new file:   index2.html
    	new file:   js/com.js
    	new file:   js/main.js
    	new file:   js/vendor/bigvideo.js
    	new file:   js/vendor/bootstrap.js
    	new file:   js/vendor/bootstrap.min.js
    	new file:   js/vendor/imagesloaded.pkgd.min.js
    	new file:   js/vendor/jquery-1.11.0.min.js
    	new file:   js/vendor/jquery-ui.min.js
    	new file:   js/vendor/modernizr-2.6.2-respond-1.1.0.min.js
    	new file:   js/vendor/video-js.swf
    	new file:   js/vendor/video.js
    	new file:   night.mp4

---
+++ js/main.js
@@ -0,0 +1,115 @@
+var date = new Date();
+			 var hour = date.getHours();
+             var min = date.getMinutes();
+
+             //console.log("hour: "+hour+" minute: "+min);
+
+             if(hour<5){
+                   $(function() {
+    			   var BV = new $.BigVideo();
+    			   BV.init();
+    			   BV.show(['night.mp4'],{ambient:true});
+                   });
+               }else if(hour<16){
+               		$(function() {
+    			   var BV = new $.BigVideo();
+    			   BV.init();
+    			   BV.show(['daylight.mp4'],{ambient:true});
+                   });
+               }else if(hour<19){
+               		$(function() {
+    			   var BV = new $.BigVideo();
+    			   BV.init();
+    			   BV.show(['evening.mp4'],{ambient:true});
+                   });
+               }else{
+                   	$(function() {
+    			   var BV = new $.BigVideo();
+    			   BV.init();
+    			   BV.show(['night.mp4'],{ambient:true});
+                   });
+ }
+
+$('.submit').click(function(){
+
+	var city = $('.city_names').val();
+	//console.log(city);
+	$.ajax({
+        url: "http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID=13ee6543faae4f2022c03420fd21df38",
+	    jsonp: "callback",
+	    dataType: "jsonp",
+	    data: {
+	        q: city,
+	        units:"metric",
+	        mode: "json"
+	    },
+	    success: function( response ) {
+             var temp = response.list[0].main.temp;
+             if(temp<25 ){
+            	 $('.message').html("it's feels cold, get a jacket");
+             }else if(temp<35){
+            	 $('.message').html("it's feels normal");
+             }else if(temp<50){
+            	 $('.message').html("it's feels hot, switch on your AC");
+             }
+
+	    	 	$('.city').html(response.city.name);
+	    	 	$('.country').html('( </span><span>'+response.city.country+'</span><span> )');
+	    	 	$('.temp').html('<span>'+response.list[0].main.temp+'</span><span class="t">* C</span>');
+
+
+	    }
+	});
+});
+
+$(document).ready(function(){
+	$( ".city_names" ).autocomplete({
+		source: function( request, response ) {
+			$.ajax({
+                url: "http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID=13ee6543faae4f2022c03420fd21df38",
+				dataType: "jsonp",
+				data: {
+					q: request.term,
+					mode: "json"
+				},
+				success: function( data ) {
+					// Fix for IE 8 as forEach function not supported yet below IE 9
+					if (typeof Array.prototype.forEach != 'function') {
+					    Array.prototype.forEach = function(callback){
+					      for (var i = 0; i < this.length; i++){
+					        callback.apply(this, [this[i], i, this]);
+					      }
+					    };
+					}
+
+					var parsed = data.list;
+					var newArray = new Array(parsed.length);
+					var i = 0;
+					  parsed.forEach(function (entry) {
+	                    var newObject = {
+	                        label: entry.name+" "+entry.sys.country
+	                    };
+	                    newArray[i] = newObject;
+	                    i++;
+	                });
+
+					  response(newArray);
+				},
+				error: function (message) {
+	                response([]);
+	            }
+			});
+		},
+		minLength: 2,
+		open: function() {
+			$( this ).removeClass( "ui-corner-all" ).addClass( "ui-corner-top" );
+		},
+		close: function() {
+			$( this ).removeClass( "ui-corner-top" ).addClass( "ui-corner-all" );
+		}
+	});
+
+});
+
+
+
